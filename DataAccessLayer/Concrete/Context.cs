61c0133757beb33336c6cd370cd745d08dd73b02
muharremyalman <yalmanmuharrem507@gmail.com>
2022-10-02 18:41:54 +0300

    Context sınıfı ve connection string

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-IT93AAM7;database=CoreBlogDb; integrated security=true;");
+
+        }
+    }
+}
