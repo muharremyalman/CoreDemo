424705bf0b28af6b1a6a27d8d5d0f7fb976eb25f
muharremyalman <yalmanmuharrem507@gmail.com>
2022-10-03 22:32:46 +0300

    11-15

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,11 @@ namespace DataAccessLayer.Concrete
             optionsBuilder.UseSqlServer("server=LAPTOP-IT93AAM7;database=CoreBlogDb; integrated security=true;");
 
         }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet <Blog> Blogs { get; set; }
+        public DbSet <Category> Categories { get; set; }
+        public DbSet <Comment> Comments { get; set; }
+        public DbSet <Contact> Contacts { get; set; }
+        public DbSet <Writer> Writers { get; set; }
     }
 }
