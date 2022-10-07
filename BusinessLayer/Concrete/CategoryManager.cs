c6c0788d11af02b720acc286a0a9cb97750866c6
muharremyalman <yalmanmuharrem507@gmail.com>
2022-10-07 19:21:13 +0300

    Business katmanı doldurulması

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+        public void CategoryAdd(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            if (category.CategoryID != 0)
+            {
+                repository.Delete(category);
+            }
+
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
